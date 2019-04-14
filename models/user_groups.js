c3f5cb437b346ef79e7d9c58da78eaa20ce0f386
N3TC4T <netcat.av@gmail.com>
2019-04-14 21:01:09 +0200

    fix bugs

--- models/user_groups.js
+++ models/user_groups.js
@@ -19,77 +19,79 @@ module.exports = function(sequelize, DataTypes) {
         let left_chat_member = null;
         let from = null;
 
-        if (ctx.updateType === 'callback_query') {
-            chat = ctx.update.callback_query.message.chat;
-            from = ctx.update.callback_query.from;
-        } else {
-            if (_.has(ctx, ['update', 'message', 'chat'])) {
-                chat = ctx.update.message.chat;
+        try {
+            if (ctx.updateType === 'callback_query') {
+                chat = ctx.update.callback_query.message.chat;
+                from = ctx.update.callback_query.from;
+            } else {
+                if (_.has(ctx, ['update', 'message', 'chat'])) {
+                    chat = ctx.update.message.chat;
+                }
+                if (_.has(ctx, ['update', 'message', 'left_chat_member'])) {
+                    left_chat_member = ctx.update.message.left_chat_member;
+                }
+                if (_.has(ctx, ['update', 'message', 'from'])) {
+                    from = ctx.update.message.from;
+                }
             }
-            if (_.has(ctx, ['update', 'message', 'left_chat_member'])) {
-                left_chat_member = ctx.update.message.left_chat_member;
-            }
-            if (_.has(ctx, ['update', 'message', 'from'])) {
-                from = ctx.update.message.from;
-            }
-        }
 
-        if (!chat || !user) {
-            return;
-        }
-        const group = await sequelize.models.Group.findOne({ where: { groupId: chat.id } });
-        if (!group) {
-            return;
-        }
-
-        if (!left_chat_member) {
-            UserGroup.findOrCreate({
-                where: {
-                    group: group.id,
-                    user: user.id,
-                },
-            });
-        } else {
-            if (!from) {
+            if (!chat || !user) {
                 return;
             }
-            if (left_chat_member.id !== from.id) {
-                // someone kicked the user
-                if (!from.is_bot) {
-                    UserGroup.findOrCreate({
-                        where: {
-                            group: group.id,
-                            user: user.id,
-                        },
-                    });
+            const group = await sequelize.models.Group.findOne({ where: { groupId: chat.id } });
+            if (!group) {
+                return;
+            }
+
+            if (!left_chat_member) {
+                UserGroup.findOrCreate({
+                    where: {
+                        group: group.id,
+                        user: user.id,
+                    },
+                });
+            } else {
+                if (!from) {
+                    return;
                 }
+                if (left_chat_member.id !== from.id) {
+                    // someone kicked the user
+                    if (!from.is_bot) {
+                        UserGroup.findOrCreate({
+                            where: {
+                                group: group.id,
+                                user: user.id,
+                            },
+                        });
+                    }
 
-                if (!left_chat_member.is_bot) {
-                    const left_user = await sequelize.models.User.findOne({
-                        where: { telegramId: left_chat_member.id },
-                    });
+                    if (!left_chat_member.is_bot) {
+                        const left_user = await sequelize.models.User.findOne({
+                            where: { telegramId: left_chat_member.id },
+                        });
 
-                    if (left_user) {
+                        if (left_user) {
+                            UserGroup.destroy({
+                                where: {
+                                    group: group.id,
+                                    user: left_user.id,
+                                },
+                            });
+                        }
+                    }
+                } else {
+                    // user left by itself
+                    if (!left_chat_member.is_bot) {
                         UserGroup.destroy({
                             where: {
                                 group: group.id,
-                                user: left_user.id,
+                                user: user.id,
                             },
                         });
                     }
                 }
-            } else {
-                // user left by itself
-                if (!left_chat_member.is_bot) {
-                    UserGroup.destroy({
-                        where: {
-                            group: group.id,
-                            user: user.id,
-                        },
-                    });
-                }
             }
-        }
+        } catch (e) {}
     };
 
     UserGroup.associate = function(models) {
